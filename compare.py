746e55b28845f2e92a4fe58d4a0656e0c99f8850
leeyunhome <leeyunhome@daum.net>
2021-12-16 23:23:57 +0900

    modify compare.py to work

--- compare.py
+++ compare.py
@@ -2,44 +2,49 @@
 # python compare.py
 
 # import the necessary packages
-from skimage.measure import structural_similarity as ssim
+# from skimage.measure import structural_similarity as ssim
+from skimage.metrics import structural_similarity as ssim
+
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
+
 def mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
+
 
 def compare_images(imageA, imageB, title):
-	# compute the mean squared error and structural similarity
-	# index for the images
-	m = mse(imageA, imageB)
-	s = ssim(imageA, imageB)
+    # compute the mean squared error and structural similarity
+    # index for the images
+    m = mse(imageA, imageB)
+    s = ssim(imageA, imageB)
 
-	# setup the figure
-	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    # setup the figure
+    fig = plt.figure(title)
+    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
 
-	# show first image
-	ax = fig.add_subplot(1, 2, 1)
-	plt.imshow(imageA, cmap = plt.cm.gray)
-	plt.axis("off")
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap=plt.cm.gray)
+    plt.axis("off")
 
-	# show the second image
-	ax = fig.add_subplot(1, 2, 2)
-	plt.imshow(imageB, cmap = plt.cm.gray)
-	plt.axis("off")
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageB, cmap=plt.cm.gray)
+    plt.axis("off")
 
-	# show the images
-	plt.show()
+    # show the images
+    plt.show()
+
 
 # load the images -- the original, the original + contrast,
 # and the original + photoshop
@@ -54,15 +59,16 @@ shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
 
 # initialize the figure
 fig = plt.figure("Images")
-images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
+images = ("Original", original), ("Contrast",
+                                  contrast), ("Photoshopped", shopped)
 
 # loop over the images
 for (i, (name, image)) in enumerate(images):
-	# show the image
-	ax = fig.add_subplot(1, 3, i + 1)
-	ax.set_title(name)
-	plt.imshow(image, cmap = plt.cm.gray)
-	plt.axis("off")
+    # show the image
+    ax = fig.add_subplot(1, 3, i + 1)
+    ax.set_title(name)
+    plt.imshow(image, cmap=plt.cm.gray)
+    plt.axis("off")
 
 # show the figure
 plt.show()
